db41387b8c39541138b785bee1fb661e172cc459
Călin Rafa <calin.rafa@alexishr.com>
2023-03-30 20:48:57 +0300

    Initial Commit

---
+++ PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by Calin Rafa on 30.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
